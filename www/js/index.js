39b4e3f4757a94ea6fd7275fd1a3057235f5be09
Rem7474 <recuv007@hotmail.fr>
2023-12-05 17:34:33 +0100

    fix bug

--- www/js/index.js
+++ www/js/index.js
@@ -5,3 +5,17 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
 }
+
+QRScanner.scan(displayContents);
+ 
+function displayContents(err, text){
+  if(err){
+    // an error occurred, or the scan was canceled (error code `6`)
+  } else {
+    // The scan completed, display the contents of the QR code:
+    alert(text);
+  }
+}
+ 
+// Make the webview transparent so the video preview is visible behind it.
+QRScanner.show();
