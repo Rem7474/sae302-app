b7cebbf8c46e3cdc36e14f6ae85abd3c20eb790c
Rem7474 <recuv007@hotmail.fr>
2023-12-11 16:26:49 +0100

    nfcreader

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,29 @@
-document.addEventListener('deviceready', onDeviceReady, false);
+//AUTHOR : Rémy CUVELIER
+//DATE : 11/12/2023
+//VERSION : 1.0
+//DESCRIPTION : Fichier javascript pour l'application mobile
+//PLUGINS : community-cordova-plugin-nfc
 
+document.addEventListener('deviceready', onDeviceReady, false);
+//document.getElementById("scan").addEventListener("click", startScan, false);
+console.log("coucou");
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById("scan").addEventListener("click", startScan, false);
+    // Vérifier si le NFC est disponible
+    nfc.enabled(
+      function() {
+          console.log("NFC est disponible!");
+          nfc.readerMode(
+            nfc.FLAG_READER_NFC_A | nfc.FLAG_READER_NO_PLATFORM_SOUNDS, 
+            nfcTag => console.log(nfc.bytesToHexString(nfcTag.id)),
+            error => console.log('NFC reader mode failed', error)
+        );
+      },
+      function() {
+          console.log("NFC n'est pas disponible sur cet appareil.");
+      }
+  );
 }
-
-QRScanner.scan(displayContents);
- 
-function displayContents(err, text){
-  if(err){
-    // an error occurred, or the scan was canceled (error code `6`)
-  } else {
-    // The scan completed, display the contents of the QR code:
-    alert(text);
-  }
-}
- 
-// Make the webview transparent so the video preview is visible behind it.
-QRScanner.show();
